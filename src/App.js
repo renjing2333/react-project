61192c598e0182b8c934e0637073211b3e9bf6e6
v_jingrren <v_jingrren@tencent.com>
2021-08-06 18:07:35 +0800

    尝试了请求数据,学习了生命周期

--- src/App.js
+++ src/App.js
@@ -3,34 +3,65 @@
 import React, {Component} from 'react'
 import './style.css'
 import InputItem from './InputItem'
+import axios from 'axios'
 
 class App extends Component{
   constructor(props){
     super(props)
     this.state = {
       inputValue: '',
-      list:['牌牌','肉肉']
+      list:['牌牌','泡芙']
     }
   }
+  // componentWillMount() {
+  //   console.log('挂载初始阶段');
+  // }
+
+  componentDidMount() {
+    console.log('挂载结束阶段');
+    axios.get('http://localhost:8888/list').then(
+      (res) => {
+        console.log('获取数据成功:' + JSON.stringify(res.data));
+        this.setState({
+          list: res.data
+        })
+      }
+    )
+  }
+
+  shouldComponentUpdate() {
+    console.log('1shouldComponentUpdate---组件发生改变前执行');
+    return true
+  }
+
+  // componentWillUpdate() {
+  //   console.log('2componentWillUpdate---组件更新前，shouldComponentUpdate函数之后执行')
+  // }
+
+  componentDidUpdate(){
+    console.log('4componentDidUpdate----组件更新之后执行')
+}
+
     render(){
+      console.log('3render---渲染阶段');
         return (
           <div className="box">
             <div>
-              <label className="labelItem" for='happy'>激活文本输入框</label>
+              <label className="labelItem" htmlFor='happy'>激活文本输入框</label>
               <input id='happy' value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
               <button onClick={this.addList.bind(this)}>添加小猫咪</button>
             </div>
-            <ul>
+            <ul ref = {(ul) => {this.ul = ul}}>
                 {
                   this.state.list.map((item,index) => {
                     // return <li key={index + item} onClick={this.deleteItem.bind(this,index)} dangerouslySetInnerHTML={{__html:item}}></li>
                     return (
-                      <div>
                         <InputItem
-                          key={index + item}
-                          content={item}
-                          index={index} />
-                      </div>
+                          key= {index + item}
+                          master = '静静家的'
+                          content= {item}
+                          index= {index}
+                          delete = {this.deleteItem.bind(this)} />
                     )
                   })
                 }
@@ -49,11 +80,12 @@ class App extends Component{
       this.setState({
         list:[...this.state.list, this.state.inputValue],
         inputValue: ''
+      },() => {
+        console.log(this.ul.querySelectorAll('div').length);
       })
     }
 
     deleteItem(index) {
-      console.log(index);
       let list = this.state.list
       list.splice(index , 1)
       this.setState({
--- src/InputItem.js
+++ src/InputItem.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+// import axios from 'axios'
 
 class InputItem extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <div onClick={this.clickItem.bind(this)}>
-                {this.props.content}
-            </div>
-        );
+  constructor(props) {
+    super(props)
+    this.clickItem = this.clickItem.bind(this)
+  }
+
+  // componentWillReceiveProps() {
+  //     console.log('componentWillReceiveProps');
+  // }
+
+  componentDidMount() {
+    // console.log('挂载结束阶段');
+    // axios.get('http://localhost:8888/list').then(
+    //   (res) => {
+    //     console.log('获取数据成功:' + JSON.stringify(res.data));
+    //   }
+    // )
+  }
+
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.content !== this.props.content) {
+      return true
+    } else {
+      return false
     }
-    clickItem() {
-        console.log('哈哈哈');
-    }
+  }
+  render() {
+    return (
+      <div onClick={this.clickItem}>
+        {this.props.master}__{this.props.content}
+      </div>
+    );
+  }
+  clickItem() {
+    this.props.delete(this.props.index)
+  }
 }
+
+InputItem.propTypes = {
+  content: PropTypes.string,
+  delete: PropTypes.func,
+  index: PropTypes.number,
+  master: PropTypes.string.isRequired
+}
+
+// InputItem.defaultProps = {
+//     master: '嘿嘿'
+// }
 
 export default InputItem;
